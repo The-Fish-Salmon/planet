52280c12e3a1353b9bffbb6114728acf87a93305
The-Fish-Salmon <tanklava2017@gmail.com>
2022-11-28 14:39:17 -0500

    stage 3

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import turtle
 
 system = System(width=1100, length=850)
 star = Star(system, mass=10000)
-planet = Planet(system, mass=1, position=(-400, 0), velocity=(0, 5))
+planet = Planet(system, mass=1, position=(-400, 0), velocity=(0, 2))
 
 while True:
     system.gravity_acc(star, planet)
